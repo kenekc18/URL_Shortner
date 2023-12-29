bf15b5afec4f15629d166dc44d16d2f04284c2ad
kenekc18 <kenekc18@gmail.com>
2023-12-29 16:29:18 +0100

    changed

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-let url_link = document.querySelector('.input');
+let url_link = document.querySelector('#url');
 let button = document.querySelector('#btn');
 let result_link = document.querySelector('.result-url')
-let apiUrl = "https://rel.ink/api/links/";
+let apiUrl = "https://api-ssl.bitly.com/v4/shorten";
 
 async function shorten(url){
     try{
-    
         let new_links = await fetch(apiUrl,{
             method: "POST",
             body: JSON.stringify({url:url}),
-            headers: {"Content-type":"application/json"}
+            headers: {
+                "Authorization": "Bearer {2d1e5578739c50899d1ecb478281d507b4b860d3}",
+                "Content-type":"application/json"
+            }
         });
         result_link.style.display = "block";
 
