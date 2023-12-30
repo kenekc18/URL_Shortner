78bbba7b1b5077bf04c93754445036ef024f1c2a
kenekc18 <kenekc18@gmail.com>
2023-12-30 02:37:27 +0100

    changes

--- script.js
+++ script.js
@@ -5,24 +5,25 @@ let apiUrl = "https://api-ssl.bitly.com/v4/shorten";
 
 async function shorten(url){
     try{
-        let new_links = await fetch(apiUrl,{
-            method: "POST",
-            body: JSON.stringify({url:url}),
+        let new_links = await fetch( apiUrl, {
+            method: 'POST',
             headers: {
-                "Authorization": "Bearer {2d1e5578739c50899d1ecb478281d507b4b860d3}",
-                "Content-type":"application/json"
-            }
+                'Authorization': 'Bearer 2d1e5578739c50899d1ecb478281d507b4b860d3',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ "long_url": url, "domain": "bit.ly", "group_guid": "BncteWHgnx8" })
         });
         result_link.style.display = "block";
 
         // IF HTTP REQUEST IS SUCESSFUL(DATA IS SUCESSFULLY GOTTEN)
         if(new_links.ok){
-            let data = await new_links.text();
-            result_link.innerHTML = `<a href ="${data}" target ="blank">${data}</a>`
+            let data = await new_links.json();
+            result_link.innerHTML = `<a href ="${data.link}" target ="_blank">${data.link}</a>`
         }    
         else{
             result_link.innerHTML = "The link could not be shortned"
             result_link.style.color = "red";
+            result_link.style.backgroundColor = "gray";
         }
         // ELSE CONDITION RUNS IF HTTP REQUEST FAILED
       
@@ -33,5 +34,6 @@ async function shorten(url){
 }
 
 button.addEventListener('click',()=>{
+    event.preventDefault();
     shorten(url_link.value)
 });                                                                                                                                                                                                                                                      
